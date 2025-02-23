f244377803fa612b2c5e4d1dc18511558f1ebbf1
LuK1337 <priv.luk@gmail.com>
2025-02-23 14:24:59 +0100

    compat: libaudioproxy: clang-format
    Change-Id: I5ee6734624676830b5d780117ff54676120d0cc1

--- libaudioproxy/libaudioproxy_shim.cpp
+++ libaudioproxy/libaudioproxy_shim.cpp
@@ -15,57 +15,57 @@
  */
 
 #include <errno.h>
-#include <unistd.h>
 #include <sound/asound.h>
+#include <unistd.h>
 
-extern "C" int get_dai_link(int /*param_1*/,int /*param_2*/) {
-  /*if (param_1 != 0) {
-    __android_log_print(2,"audio_route","requested PCM for %d",param_2);
-  }*/
-  return -1;
+extern "C" int get_dai_link(int /*param_1*/, int /*param_2*/) {
+    /*if (param_1 != 0) {
+      __android_log_print(2,"audio_route","requested PCM for %d",param_2);
+    }*/
+    return -1;
 }
 
 // possibly audio_route_missing_ctl(struct audio_route *ar)
-extern "C" int audio_route_missing_ctl(int *param_1) {
-  if (param_1 == 0) {
-    //__android_log_print(6,"audio_route","invalid audio_route");
-    return 0;
-  }
-  int a = *(int *)(param_1 + 6);
-  return a;
+extern "C" int audio_route_missing_ctl(int* param_1) {
+    if (param_1 == 0) {
+        //__android_log_print(6,"audio_route","invalid audio_route");
+        return 0;
+    }
+    int a = *(int*)(param_1 + 6);
+    return a;
 }
 
 struct mixer {
     int fd;
     struct snd_ctl_card_info card_info;
-    struct snd_ctl_elem_info *elem_info;
-    struct mixer_ctl *ctl;
+    struct snd_ctl_elem_info* elem_info;
+    struct mixer_ctl* ctl;
     unsigned int count;
 };
 
 struct audio_route {
-    struct mixer *mixer;
+    struct mixer* mixer;
     unsigned int num_mixer_ctls;
-    struct mixer_state *mixer_state;
+    struct mixer_state* mixer_state;
 
     unsigned int mixer_path_size;
     unsigned int num_mixer_paths;
-    struct mixer_path *mixer_path;
+    struct mixer_path* mixer_path;
 };
 
-extern "C" int * mixer_read_event(struct mixer *mixer, uint param_2) {
-    struct snd_ctl_event *ev = NULL;
+extern "C" int* mixer_read_event(struct mixer* mixer, uint param_2) {
+    struct snd_ctl_event* ev = NULL;
     ssize_t count;
 
     if (mixer && mixer->fd >= 0) {
-        ev = (struct snd_ctl_event *)calloc(1, sizeof(*ev));
+        ev = (struct snd_ctl_event*)calloc(1, sizeof(*ev));
 
         if (ev) {
             count = read(mixer->fd, ev, sizeof(*ev));
 
             while (0 < count) {
                 if (ev->type == 0 && (ev->data.elem.mask & param_2) != 0) {
-                    return (int *)ev;
+                    return (int*)ev;
                 }
 
                 count = read(mixer->fd, ev, sizeof(*ev));
